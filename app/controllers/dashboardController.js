fc7dc155d10ff780dae432de9bbbfb5b6919b11a
naimish bhagat <naimish.bhagat@arq.group>
2020-05-09 16:50:43 +1000

    reorder added

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,10 +1,9 @@
-mcDonaldApp.controller('dashboardController', ['$scope','$state', '$http','$location','$localStorage','$window','$cookies','OrderFactory',
-    function ($scope,$state, $http,$location,$localStorage,$window,$cookies,OrderFactory) {
+mcDonaldApp.controller('dashboardController', ['$scope','$state', '$http','$location','$localStorage','$filter','$cookies','OrderFactory',
+    function ($scope,$state, $http,$location,$localStorage,$filter,$cookies,OrderFactory) {
         $scope.userInfo = $localStorage.userInfo;
         $scope.user = {};
 
         OrderFactory.load_orders().then(function (response) {
-            console.log(response);
             $scope.myOrders= response.list;
         });
 
@@ -13,6 +12,13 @@ mcDonaldApp.controller('dashboardController', ['$scope','$state', '$http','$loca
         };
 
         $scope.reorder_meal = function (id) {
+            if ($localStorage.cart == 'undefined' || $localStorage.cart == null) {
+                $localStorage.cart = [];
+            }
+            var order =  $filter('filter')( $scope.myOrders, {id: id},true);
+            var selectedMenu = order[0].meal;
+            console.log(selectedMenu);
+            $localStorage.cart.push(selectedMenu);
 
         }
     }
